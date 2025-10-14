e03f95ba53fecce36511f399dfef20f451bb059f
James Brown <jbrow164@asu.edu>
2025-10-14 20:26:24 +0900

    Created rotating body to simulate orbiting Pschye

---
+++ Assets/Scripts/orbitPschye.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class orbitPschye : MonoBehaviour
+{
+    [SerializeField] private Transform obj;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //sets Pschye to orginal coordinates at the start of the game
+        obj.Rotate(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //has Pschye rotate counterclockwise to simulate orbiting
+        obj.Rotate(0, -0.25f, 0);   
+    }
+}
