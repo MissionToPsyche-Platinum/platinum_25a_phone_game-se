f58d6998ef94a208e7c6879598b9c7be6ecc90e2
Salem Bitney <sbitney@asu.edu>
2025-10-19 17:25:56 -0700

    Task 90 - Implement grid input consequences

--- Assets/Scripts/MinigameAMarkGrid.cs
+++ Assets/Scripts/MinigameAMarkGrid.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinigameAMarkGrid : MonoBehaviour
 {
     [SerializeField] private GameObject mark;
     private bool isMarked;
+    [SerializeField] private MinigameAToggle toggle;
+    [SerializeField] private bool check;
 
     public void ShowMark()
     {
         if (!isMarked)
         {
+            if (toggle.IsCheck())
+            {
+                if (!check)
+                {
+                    mark.GetComponent<Image>().color = Color.red;
+                }
+            }
+            else
+            {
+                if (check)
+                {
+                    mark.GetComponent<Image>().color = Color.red;
+                }
+            }
             mark.SetActive(true);
             //later, add points to player score
         }
