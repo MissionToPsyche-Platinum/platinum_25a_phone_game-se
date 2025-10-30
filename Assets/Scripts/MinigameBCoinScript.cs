52fa70be4d3f7855741e1ffeb5ed454d43da5a6e
Salem Bitney <sbitney@asu.edu>
2025-10-30 15:48:37 -0700

    Task 116 - create basic UI for Minigame B Metal Weights

---
+++ Assets/Scripts/MinigameBCoinScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameBCoinScript : MonoBehaviour
+{
+    [SerializeField] private int coinWeight;
+
+    public int CoinWeight { get { return coinWeight; } }
+    
+}
