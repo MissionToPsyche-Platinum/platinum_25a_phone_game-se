7bf07489353443aa61eb45fdf4918b58ef0df7b4
Brianna Browning <you@example.com>
2026-01-15 17:19:03 -0700

    Implement jump ring mechanics
    - Velocity is set to double of the 'max' velocity for the level upon interaction with a jump ring
    
    - All collisions are disabled while the velocity is greater than the 'max' velocity
    
    - After 3 seconds, the velocity returns to 'max'
    
    Note: The duration is determined mathematically by increasing the gravity, so that the velocity returns to 'max' after 3 seconds. This method may change to set the velocity back to 'max' after 3 seconds as the current method might not feel like as much of a 'buff' as intended. This will be determined upon further testing in level development.

--- Assets/Scripts/MinigameD/Level1/RocketVelocity.cs
+++ Assets/Scripts/MinigameD/Level1/RocketVelocity.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class RocketVelocity : MonoBehaviour
 {
-    public float initialVelocity = 15f; 
-    public float gravity = -5f;
+    public float initialVelocity = 15f;
+    public float initialGravity = -2.5f;
 
     private float currentVelocity;
+    private float currentGravity;
     private bool ascending = true;
+    private bool collisionEnabled = true;
 
     void Start()
     {
         currentVelocity = initialVelocity;
+        currentGravity = initialGravity;
     }
 
     void FixedUpdate()
     {
+        // checks if jump ring is enabled
+        if (Mathf.Abs(currentVelocity) > initialVelocity)
+        {
+            // disable collision
+            collisionEnabled = false;
+
+            // set gravity to one-third of initialVelocity, so that invulnerability ends after 3 seconds
+            currentGravity = -(initialVelocity / 3f);
+        } else
+        {
+            // re-enable collision once velocity is back to normal
+            if (!collisionEnabled)
+            {
+                collisionEnabled = true;
+                currentGravity = initialGravity; // reset to original gravity
+            }
+        }
+
         if (ascending)
         {
-            currentVelocity += gravity * Time.deltaTime;
+            currentVelocity += currentGravity * Time.deltaTime;
             transform.position += new Vector3(0, currentVelocity * Time.deltaTime, 0);
 
             // check if velocity has reached zero (peak)
@@ -32,7 +54,7 @@ public class RocketVelocity : MonoBehaviour
         else
         {
             // falling down
-            currentVelocity += gravity * Time.deltaTime;
+            currentVelocity += currentGravity * Time.deltaTime;
             transform.position += new Vector3(0, currentVelocity * Time.deltaTime, 0);
 
             // check if velocity is positive again (ring interaction)
@@ -45,6 +67,9 @@ public class RocketVelocity : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!collisionEnabled)
+            return; // ignore collisions when disabled
+
         if (other.CompareTag("BoostRing"))
         {
             boostVelocity();
@@ -57,18 +82,29 @@ public class RocketVelocity : MonoBehaviour
 
             other.gameObject.SetActive(false);
         }
+        else if (other.CompareTag("JumpRing"))
+        {
+            doubleVelocity();
+
+            other.gameObject.SetActive(false);
+        }
     }
 
-    // triggers when interacting with boost ring
+    // boost ring: stabilize velocity
     public void boostVelocity()
     {
         currentVelocity = initialVelocity;
     }
 
-    // triggers when interacting with penalty ring
+    // penalty ring: decrease velocity by 25%
     public void decreaseVelocity()
     {
-        // decrease by 25%
         currentVelocity = currentVelocity * (float)0.75;
+    }
+
+    // jump ring: double velocity + start invulnerability
+    public void doubleVelocity()
+    {
+        currentVelocity = initialVelocity * 2;
     }
 }
