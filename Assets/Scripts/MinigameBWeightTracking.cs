f44bbb71f65044775fe6c901cffad663a3bd985c
Salem Bitney <sbitney@asu.edu>
2026-01-24 15:14:16 -0800

    Task 248 - Add Randomization to Minigame B Puzzle Metal Weights

--- Assets/Scripts/MinigameBWeightTracking.cs
+++ Assets/Scripts/MinigameBWeightTracking.cs
@@ -23,6 +23,15 @@ public class MinigameBWeightTracking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        int lightWeight = Random.Range(2, 5); // 2-4
+        int orangeWeight = Random.Range(4, 9); // 4-8
+        int darkWeight = Random.Range(9, 13); // 9-12
+        lightCoin.GetComponent<MinigameBCoinScript>().CoinWeight = lightWeight;
+        orangeCoin.GetComponent<MinigameBCoinScript>().CoinWeight = orangeWeight;
+        darkCoin.GetComponent<MinigameBCoinScript>().CoinWeight = darkWeight;
+        goalWeightInt = Random.Range(0, 6) * lightWeight + Random.Range(0, 6) * orangeWeight + Random.Range(0, 6) * darkWeight;
+        goalWeight.text = goalWeightInt.ToString();
         // this is where we'll generate randomness later.
     }
 
