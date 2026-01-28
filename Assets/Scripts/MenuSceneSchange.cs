5c0041b681b5db6f4739a272fe5a7e5ebec209d0
Salem W <salem.w.winters@gmail.com>
2026-01-27 19:19:18 -0800

    Add connection to central hub into dev (#21)

--- Assets/Scripts/MenuSceneSchange.cs
+++ Assets/Scripts/MenuSceneSchange.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuSceneSchange : MonoBehaviour
 {
+    [SerializeField] private GameObject UnavailableScreen;
+
     public void SwitchToSceneMinigameA()
     {
         Debug.Log("Switching to Minigame A");
@@ -13,26 +15,45 @@ public class MenuSceneSchange : MonoBehaviour
 
     public void SwitchToSceneMinigameB()
     {
-
+        Debug.Log("Switching to Minigame B");
+        Random.InitState(System.DateTime.Now.Millisecond);
+        int rand = Random.Range(0, 2);
+        if (rand == 0)
+        {
+            SceneManager.LoadScene("MinigameBMetalWeights");
+        }
+        else
+        {
+            SceneManager.LoadScene("MinigameBPowerBalance");
+        }
     }
 
     public void SwitchToSceneMinigameC()
     {
-
+        Debug.Log("Switching to Minigame C");
+        UnavailableScreen.SetActive(true);
     }
 
     public void SwitchToSceneMinigameD()
     {
-
+        Debug.Log("Switching to Minigame D");
+        SceneManager.LoadScene("MinigameD-Tutorial");
     }
 
     public void SwitchToSceneMinigameE()
     {
-
+        Debug.Log("Switching to Minigame E");
+        SceneManager.LoadScene("GravityAssist");
     }
 
     public void SwitchToSceneMinigameF()
     {
+        Debug.Log("Switching to Minigame F");
+        UnavailableScreen.SetActive(true);
+    }
 
+    public void CloseUnavailableScreen()
+    {
+        UnavailableScreen.SetActive(false);
     }
 }
