37823d362189699c1e563468c7e7bdc3ab7c3dd6
jbrow164 <jbrow164@asu.edu>
2026-02-10 13:28:16 -0600

    When added Minigame F will be added to the dev branch  (#27)
    * correcting branch to align with main branch
    
    * Correcting branch to align with main branch
    
    * Created rotating body to simulate orbiting Pschye
    
    * Corrected sphere mesh
    
    * added mesh for sphere
    
    * Created RedBubble Spawning
    
    * Added blue bubble spawns and slowed spawns
    
    * Added green bubble spawner and corrected rotation of spwans
    
    * Added mouse controls to game
    
    * Added comments to code
    
    * Created Background space for game and made space spin
    
    * Fixed MinigameF to work in correct Unity Version
    
    * Corrected Spawns to be more on sphere and limited spawn collisions
    
    * Added particle animation when collecting science
    
    * Added UI points element
    
    * corrected points added mechanics
    
    * Corrected files to work with dev branch
    
    * Created Space Texture for MinigameF
    
    * Made science collection targets
    
    * add correct pre-fabs to folder for science collection
    
    * Added Psyche Satellite to UI
    
    * Added art to satellite and asteroid
    
    * Fixed Names on satellite components
    
    * Added MiniGameF to the CentralHub
    
    * Fixed Models and prefabs for game to run as intended
    
    * Fixed StageManager to work as intended
    
    * merged dev branch into current branch
    
    * Created Start Screen for mingameF
    
    * Added Options for start menu
    
    * Added difficulty option to start menu
    
    * Added menu and quit resume actions to minigameF
    
    * Fixed bug where you can still collect points with menu paused
    
    * Made quit button return to centralhub
    
    * Added end screen with transitions to central hub
    
    * add minigame_F start to menus election script
    
    * created obstacles to make game more interesting
    
    * game objects added to work with obstacles
    
    * minor bug fixes with obstacles
    
    * added transition between start screen and game
    
    * created transitions between each stage of the game
    
    * fixed bug where science would not spawn after transistions
    
    * Created Long Tutorial for minigameF
    
    * Merged dev into branch to make a fast forward possible
    
    * Fixed Bugs created by fastforward
    
    * Added scaling difficulty when stage increases
    
    * added increase in speed as you increase in stage
    
    * Started work on Task #310
    
    * Completed Task 308
    
    * started countdown timer task
    
    * Created countdown timer for minigameF
    
    * Assets created for countdown timer for minigameF
    
    * Fixed Spawn mechanics during scene transition
    
    * Prevented science collection when science was behind the asteroid
    
    * Fixed scale to further prevent collection of science behind asteroid
    
    * Corrected science mechanics to not  have 2 spawned on top of each other
    
    * Added Background Audio and animation Audio
    
    * Merged dev into branch for fastforward

---
+++ Assets/Scripts/MinigameF/collectRedScience.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class collectPoints : MonoBehaviour
+{
+    public float pointsAdded = 1f;
+    public ParticleSystem clickParticles;
+    [SerializeField] public PointTracker pt;
+    [SerializeField] AudioSource audio;
+
+    [SerializeField] GameObject asteroid;
+    GameObject test;
+    
+    
+
+    private UnityAction addPoints;
+    
+
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()  
+    {
+        
+     
+    }
+
+
+    //detects click of a mouse or touchscreen input and collects the science of the redbubble and adds 1 point
+    //works with mouse and touch screen
+    public void OnMouseDown()
+    {
+       
+
+        
+            pointsAdded = 1;
+            Debug.Log("Points added: " + pointsAdded);
+
+            if (Time.timeScale == 1)
+            {
+                if (clickParticles != null)
+                {
+                    clickParticles.transform.position = transform.position;
+                    clickParticles.Play();
+                    audio.Play();
+                }
+
+
+                pt.addPoints(pointsAdded);
+
+
+                Destroy(gameObject);
+            }
+        
+
+    }    
+
+    
+}
---
+++ Assets/moveHorz.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveHorz : MonoBehaviour
+{
+    [SerializeField] Transform obj1;
+    
+
+    bool obj1direction = true;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (obj1.position.x >= 6)
+        {
+            obj1direction = false;
+        }
+
+        if (obj1.position.x <= -6)
+        {
+            obj1direction = true;
+        }
+
+        
+
+        movehorz(obj1, obj1.position.x,obj1.position.y, obj1.position.z, obj1direction);
+        
+
+    }
+
+    void movehorz(Transform tob, float x,float y,float z, bool dir)
+    {
+        
+        
+        float change = 0.1f;
+
+        if (dir)
+        {
+            tob.position = new Vector3(x + change,y , z);
+            
+        }
+        else if (!dir)
+        {
+            tob.position = new Vector3(x - change, y, z);
+            
+        }
+
+    }
+}
