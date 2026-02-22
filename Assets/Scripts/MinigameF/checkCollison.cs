c7334d6b3840872373d859c20f9dc93848678af8
jbrow164 <jbrow164@asu.edu>
2026-02-21 20:06:57 -0600

    Updating Minigame F in dev branch (#33)
    * correcting branch to align with main branch
    
    * Correcting branch to align with main branch
    
    * Created rotating body to simulate orbiting Pschye
    
    * Corrected sphere mesh
    
    * added mesh for sphere
    
    * Created RedBubble Spawning
    
    * Added blue bubble spawns and slowed spawns
    
    * Added green bubble spawner and corrected rotation of spwans
    
    * Added mouse controls to game
    
    * Added comments to code
    
    * Created Background space for game and made space spin
    
    * Fixed MinigameF to work in correct Unity Version
    
    * Corrected Spawns to be more on sphere and limited spawn collisions
    
    * Added particle animation when collecting science
    
    * Added UI points element
    
    * corrected points added mechanics
    
    * Corrected files to work with dev branch
    
    * Created Space Texture for MinigameF
    
    * Made science collection targets
    
    * add correct pre-fabs to folder for science collection
    
    * Added Psyche Satellite to UI
    
    * Added art to satellite and asteroid
    
    * Fixed Names on satellite components
    
    * Added MiniGameF to the CentralHub
    
    * Fixed Models and prefabs for game to run as intended
    
    * Fixed StageManager to work as intended
    
    * merged dev branch into current branch
    
    * Created Start Screen for mingameF
    
    * Added Options for start menu
    
    * Added difficulty option to start menu
    
    * Added menu and quit resume actions to minigameF
    
    * Fixed bug where you can still collect points with menu paused
    
    * Made quit button return to centralhub
    
    * Added end screen with transitions to central hub
    
    * add minigame_F start to menus election script
    
    * created obstacles to make game more interesting
    
    * game objects added to work with obstacles
    
    * minor bug fixes with obstacles
    
    * added transition between start screen and game
    
    * created transitions between each stage of the game
    
    * fixed bug where science would not spawn after transistions
    
    * Created Long Tutorial for minigameF
    
    * Merged dev into branch to make a fast forward possible
    
    * Fixed Bugs created by fastforward
    
    * Added scaling difficulty when stage increases
    
    * added increase in speed as you increase in stage
    
    * Started work on Task #310
    
    * Completed Task 308
    
    * started countdown timer task
    
    * Created countdown timer for minigameF
    
    * Assets created for countdown timer for minigameF
    
    * Fixed Spawn mechanics during scene transition
    
    * Prevented science collection when science was behind the asteroid
    
    * Fixed scale to further prevent collection of science behind asteroid
    
    * Corrected science mechanics to not  have 2 spawned on top of each other
    
    * Added Background Audio and animation Audio
    
    * Merged dev into branch for fastforward
    
    * Updated MenuSceneSchange to match dev branch
    
    * Started work on fixing spawn mechanics for minigame F
    
    * Corrected spawn mechanics to work with later improvements to minigame F
    
    * started work on integrating difficulty levels in minigameF
    
    * Continued working on creating difficulty singleton to pass between scenes
    
    * Created are you sure prompt for minigame F
    
    * Fixed spelling of Psyche in minigame f start menu and added images to icons
    
    * Fixed aspect ratio in minigame F start screen
    
    * Fixed Aspect ratio of minigame F
    
    * Fixed difficulty settings in minigame F
    
    * Another fix of start screen UI to work with simulators
    
    * Refixed minigame F's UI to work with simulators
    
    * Corrected rotation mechanics
    
    * worked on spawn mechanics
    
    * For minigame F made it so only 1 difficulty option can be selected at a time
    
    * Fixed bugs with central hub
    
    * Fixed back button in MiniGame B weights and got rid of unneeded classes for minigame F
    
    * added minigame F to build settings
    
    * adjusted mini game f spawn rate to be faster
    
    * Fix Psyche spelling error
    
    ---------
    
    Co-authored-by: Salem Bitney <sbitney@asu.edu>

---
+++ Assets/Scripts/MinigameF/checkCollison.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class checkCollison : MonoBehaviour
+{
+    private float check = 0.000005f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
+             
+
+        if(collision.gameObject.name == "RedScience(Clone)" || collision.gameObject.name == "GreenScience(Clone)" || collision.gameObject.name == "BlueScience(Clone)" )
+        {
+
+            if (gameObject.name == "RedScience(Clone)" || gameObject.name == "GreenScience(Clone)" || gameObject.name == "BlueScience(Clone)")
+            {
+
+                if (gameObject.name == "RedScience(Clone)" && collision.gameObject.name == "GreenScience(Clone)")
+                {
+                    if (collision.gameObject.transform.position.z - gameObject.transform.position.z < check)
+                    {
+                        Destroy(gameObject, 2f);
+                    }
+
+                } else if (gameObject.name == "GreenScience(Clone)" && collision.gameObject.name == "BlueScience(Clone)")
+                {
+                    if (collision.gameObject.transform.position.z - gameObject.transform.position.z < check)
+                    {
+                        Destroy(gameObject, 2f);
+                    }
+
+                } else if (gameObject.name == "BlueScience(Clone)" && collision.gameObject.name == "RedScience(Clone)")
+                {
+                    if (collision.gameObject.transform.position.z - gameObject.transform.position.z < check)
+                    {
+                        Destroy(gameObject, 2f);
+                    }
+                } else if (gameObject.name == collision.gameObject.name)
+                {
+                    if (collision.gameObject.transform.position.z - gameObject.transform.position.z < check)
+                    {
+                        Destroy(gameObject, 2f);
+                    }
+                }
+
+                   
+            }
+        }
+    }
+
+    public void setCheck(float check)
+    {
+        this.check = check;
+    }
+    
+}
