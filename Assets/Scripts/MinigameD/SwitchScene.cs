2c8b54779c076c64a9bd74a325be08804ebcfa41
Brianna Browning <you@example.com>
2026-04-03 15:44:09 -0700

    Add tutorial setting to main menu
    - Added persistent tutorial enabling/disabling to minigame D's main menu

--- Assets/Scripts/MinigameD/SwitchScene.cs
+++ Assets/Scripts/MinigameD/SwitchScene.cs
@@ -4,10 +4,20 @@ using UnityEngine.SceneManagement;
 public class SwitchScene : MonoBehaviour
 {
     public string sceneName;
+    private const string PREF_TUT_KEY = "TutorialsOn";
+    private bool tutorial = true; // assume tutorial enabled
 
     public void ChangeScene()
     {
         MinigameD_AudioManager.Instance.buttonClick();
-        SceneManager.LoadScene(sceneName);
+
+        tutorial = PlayerPrefs.GetInt(PREF_TUT_KEY, tutorial ? 1 : 0) == 1;
+        if (sceneName == "MinigameD-Tutorial" && !tutorial) // tutorial is disabled
+        {
+            SceneManager.LoadScene("MinigameD-Writing");
+        } else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
