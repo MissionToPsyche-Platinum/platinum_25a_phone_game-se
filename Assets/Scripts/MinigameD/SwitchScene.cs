cb611921c423aaa0686dbb1d64b8477f9dce4f3a
Brianna Browning <you@example.com>
2026-04-11 17:26:43 -0700

    Added a story disable/enable for minigame D
    - Separated settings panel script from central hub
    
    - Added a toggle in the minigame D main menu that enables/disables the story content

--- Assets/Scripts/MinigameD/SwitchScene.cs
+++ Assets/Scripts/MinigameD/SwitchScene.cs
@@ -4,18 +4,42 @@ using UnityEngine.SceneManagement;
 public class SwitchScene : MonoBehaviour
 {
     public string sceneName;
+
     private const string PREF_TUT_KEY = "TutorialsOn";
+    private const string PREF_STORY_KEY = "MinigameD-Story";
+
     private bool tutorial = true; // assume tutorial enabled
+    private bool story = true; // assume story is enabled
 
     public void ChangeScene()
     {
         MinigameD_AudioManager.Instance.buttonClick();
 
         tutorial = PlayerPrefs.GetInt(PREF_TUT_KEY, tutorial ? 1 : 0) == 1;
-        if (sceneName == "MinigameD-Tutorial" && !tutorial) // tutorial is disabled
+        story = PlayerPrefs.GetInt(PREF_STORY_KEY, story ? 1 : 0) == 1;
+
+        if (sceneName == "MinigameD-Tutorial" && !tutorial)
         {
-            SceneManager.LoadScene("MinigameD-Writing");
-        } else
+            if (story)
+            {
+                SceneManager.LoadScene("MinigameD-Writing");
+            } else
+            {
+                SceneManager.LoadScene("MinigameD-Level1");
+            }
+        } 
+        else if (sceneName == "MinigameD-Level1")
+        {
+            if (story)
+            {
+                SceneManager.LoadScene("MinigameD-Writing");
+            }
+            else
+            {
+                SceneManager.LoadScene("MinigameD-Level1");
+            }
+        }
+        else
         {
             SceneManager.LoadScene(sceneName);
         }
